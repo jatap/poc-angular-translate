c3d976878a10a1066fb2e3f9258782201afbbda1
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2016-05-02 19:20:58 +0100

    First sending .

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('pocTranslate')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
